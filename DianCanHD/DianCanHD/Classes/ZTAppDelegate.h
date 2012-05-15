95195d63e9cd2f70c49dd1f804789e775d82d643
李炜 <apple@localhost.(none)>
2012-05-15 10:45:54 +0800

    创建项目

---
+++ DianCanHD/DianCanHD/Classes/ZTAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ZTAppDelegate.h
+//  DianCanHD
+//
+//  Created by 李炜 on 12-5-15.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ZTViewController;
+
+@interface ZTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ZTViewController *viewController;
+
+@end
---
+++ DianCanHD/DianCanHD/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DianCanHD
+//
+//  Created by 李炜 on 12-5-15.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZTAppDelegate class]));
+    }
+}
